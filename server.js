3435c2536f179ced4a3d6f8d7727783a0ce3951e
Rob Ede <robjtede@icloud.com>
2017-07-16 18:39:17 +0100

    add XSS prevention

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const { parse } = require("querystring");
 
+const Entities = require("html-entities").XmlEntities;
+
 const Koa = require("koa");
 const Router = require("koa-router");
 
 const app = new Koa();
 const router = new Router();
 
+const entities = new Entities();
+
 router.get("/auth/", async (ctx, next) => {
   await next();
 
   const { search } = ctx.req._parsedUrl;
+  const safeSearch = entities.encode(search);
 
   ctx.body = `
     <style>
@@ -25,13 +30,16 @@ router.get("/auth/", async (ctx, next) => {
     </style>
     
     <p>
-      <a href="monux://auth/${search}">
+      <a href="monux://auth/${safeSearch}">
         Click here to log in to Monux &rarr;
       </a>
     </p>
     
     <p>
       This authentication proxy does NOT store or use any of your data.
+    </p>
+    <p>
+      It is not possible to make requests on your behalf since your "Client secret" is not sent though this proxy and the secret is required to request an access token.
     </p>
   `;
   ctx.status = 200;
